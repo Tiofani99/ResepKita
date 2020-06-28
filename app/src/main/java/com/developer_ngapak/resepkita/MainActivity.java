a4d2d6c7486813df676705def410f0763beb38bd
Tiofani99 <tiofani03@gmail.com>
2020-06-28 20:54:55 +0700

    mengganti dengan butterknife

--- app/src/main/java/com/developer_ngapak/resepkita/MainActivity.java
+++ app/src/main/java/com/developer_ngapak/resepkita/MainActivity.java
@@ -1,7 +1,5 @@
 package com.developer_ngapak.resepkita;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -12,16 +10,23 @@ import com.developer_ngapak.resepkita.ui.register.RegisterActivity;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import androidx.appcompat.app.AppCompatActivity;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private Button btnLogin, btnRegister;
+    @BindView(R.id.btn_login_start)
+    Button btnLogin;
+    @BindView(R.id.btn_register_start)
+    Button btnRegister;
     FirebaseUser firebaseUser;
 
     @Override
     protected void onStart() {
         super.onStart();
         firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
-        if(firebaseUser != null){
+        if (firebaseUser != null) {
             Intent intent = new Intent(MainActivity.this, DashboardActivity.class);
             startActivity(intent);
             finish();
@@ -32,8 +37,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btnLogin = findViewById(R.id.btn_login_start);
-        btnRegister = findViewById(R.id.btn_register_start);
+        ButterKnife.bind(this);
         btnLogin.setOnClickListener(this);
         btnRegister.setOnClickListener(this);
     }
@@ -41,7 +45,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View view) {
         int id = view.getId();
-        switch (id){
+        switch (id) {
             case R.id.btn_login_start:
                 Intent login = new Intent(this, LoginActivity.class);
                 startActivity(login);
