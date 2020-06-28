e4ac74dc46ecc40e96ea924c2c46056163557a87
Tiofani99 <tiofani03@gmail.com>
2020-06-28 15:29:34 +0700

    menghapus kode yang tidak perlu

--- app/src/main/java/com/developer_ngapak/resepkita/ui/fragment/dashboard/DashboardFragment.java
+++ app/src/main/java/com/developer_ngapak/resepkita/ui/fragment/dashboard/DashboardFragment.java
@@ -8,18 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.widget.SearchView;
-import androidx.fragment.app.Fragment;
-
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
-
 import com.bumptech.glide.Glide;
-
 import com.developer_ngapak.resepkita.R;
 import com.developer_ngapak.resepkita.adapter.FireBaseViewHolder;
 import com.developer_ngapak.resepkita.adapter.GridFoodAdapter;
@@ -28,17 +17,22 @@ import com.developer_ngapak.resepkita.ui.add_recipe.AddRecipeActivity;
 import com.developer_ngapak.resepkita.ui.detail.DetailActivity;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
-
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-
 import com.google.firebase.database.ValueEventListener;
 
-
 import java.util.ArrayList;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.SearchView;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 
 public class DashboardFragment extends Fragment {
@@ -47,19 +41,15 @@ public class DashboardFragment extends Fragment {
     private DatabaseReference databaseReference;
 
     private SwipeRefreshLayout swipeRefreshLayout;
-    private ArrayList<Food> list = new ArrayList<>();
     private FirebaseRecyclerOptions<Food> options;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         final View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
         FloatingActionButton btnAdd = root.findViewById(R.id.fab);
-        btnAdd.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getActivity(), AddRecipeActivity.class);
-                startActivity(intent);
-            }
+        btnAdd.setOnClickListener(view -> {
+            Intent intent = new Intent(getActivity(), AddRecipeActivity.class);
+            startActivity(intent);
         });
         swipeRefreshLayout = root.findViewById(R.id.swLayout);
 
@@ -84,7 +74,6 @@ public class DashboardFragment extends Fragment {
             }
         });
 
-
         return root;
     }
 
@@ -100,24 +89,11 @@ public class DashboardFragment extends Fragment {
                         .load(model.getImg())
                         .into(holder.img);
 
-                holder.itemView.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        final String cName = getItem(position).getName();
-                        final String cDetail = getItem(position).getDetail();
-                        final String cIngredient = getItem(position).getIngredient();
-                        final String cRecipe = getItem(position).getRecipe();
-                        final String cImage = getItem(position).getImg();
-
-                        Intent intent = new Intent(getContext(), DetailActivity.class);
-                        intent.putExtra(DetailActivity.EXTRA_NAME, cName);
-                        intent.putExtra(DetailActivity.EXTRA_DESC, cDetail);
-                        intent.putExtra(DetailActivity.EXTRA_ALAT, cIngredient);
-                        intent.putExtra(DetailActivity.EXTRA_PROCESS, cRecipe);
-                        intent.putExtra(DetailActivity.EXTRA_IMAGE, cImage);
-                        startActivity(intent);
-
-                    }
+                holder.itemView.setOnClickListener(view -> {
+                    Food food = getItem(position);
+                    Intent intent = new Intent(getContext(), DetailActivity.class);
+                    intent.putExtra(DetailActivity.EXTRA_RECIPE, food);
+                    startActivity(intent);
                 });
 
             }
@@ -142,18 +118,10 @@ public class DashboardFragment extends Fragment {
 
     private void refreshData() {
         swipeRefreshLayout.setColorSchemeColors(getResources().getColor(R.color.colorAccent2), getResources().getColor(R.color.colorPrimary2));
-        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-                                                    @Override
-                                                    public void onRefresh() {
-                                                        new Handler().postDelayed(new Runnable() {
-                                                            @Override
-                                                            public void run() {
-                                                                swipeRefreshLayout.setRefreshing(false);
-                                                                showData();
-                                                            }
-                                                        }, 3000);
-                                                    }
-                                                }
+        swipeRefreshLayout.setOnRefreshListener(() -> new Handler().postDelayed(() -> {
+                    swipeRefreshLayout.setRefreshing(false);
+                    showData();
+                }, 3000)
         );
     }
 
@@ -175,6 +143,7 @@ public class DashboardFragment extends Fragment {
                         rvFood.setLayoutManager(new GridLayoutManager(getActivity(), 1));
                         rvFood.setAdapter(cardViewAdapter);
                     }
+
                     @Override
                     public void onCancelled(@NonNull DatabaseError databaseError) {
                     }
